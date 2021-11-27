f13b7f1554a9ddf1d1317115c56584af8c758572
bitSheriff <bitSheriff@protonmail.com>
2021-11-27 18:13:26 +0100

    added qtimer for periodic function

--- lib/nordvpn.py
+++ lib/nordvpn.py
@@ -7,7 +7,8 @@ class nordvpn():
 
     ##
     # @brief    Check Nordvpn installation
-    # @details  this interface is used to get if nordvpn-cli is installed on this system
+    # @details  This interface is used to get if nordvpn-cli is installed on this system
+    #
     # @returns  Returns boolish value if nordvpn-cli is installed
     # @retval   False   nordvpn is not installed
     # @retval   True    nordvpn is installed
@@ -18,6 +19,13 @@ class nordvpn():
             installed = True
         return installed
 
+    ##
+    # @brief    Check Nordvpn connection
+    # @details  This interface is used to get if the user is currently connected to the nordvpn servers
+    #
+    # @returns  Returns boolish value if nordvpn-cli is connected
+    # @retval   False   nordvpn is not connected
+    # @retval   True    nordvpn is connected
     def isConnected(self):
         connected = False
         res = self.__getOSString("nordvpn status")
@@ -25,9 +33,24 @@ class nordvpn():
             connected = True
         return connected
 
+    ## 
+    # @public
+    # @brief    Connect to the vpn
+    # @details  This interface is used to connect to a nordvpn server. 
+    #           With the given parameter it can be defined which country and city is used.
+    # @note     If country and city are not given then the nordvpn-cli will use the default one.
+    #
+    # @param    cnt    Wanted country
+    # @param    cty    Wanted city
+    def connect(self, cnt="", cty=""):
+        return
 
 
- # # # # # PRIVATE FUNCTIONS # # # # # 
+    ## 
+    # @private
+    # @brief    Get string from a OS command
+    #
+    # @returns  Returns the string wich the OS returned on the stdout
     def __getOSString(cmnd):
         call = os.popen(cmnd, 'r')
         return call.read()
--- vizu/mainWindow.py
+++ vizu/mainWindow.py
@@ -12,12 +12,12 @@ from lib.nordvpn import nordvpn as nordvpn
 uiFile = "vizu/norsevpn.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(uiFile)
 
+
 ##
 # @brief mainWindow class
-# 
-# @param QtWidgets.QMainWindow
-# @param Ui_MainWindow
 class mainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+
+
 
     ##
     # @brief    Init
@@ -30,9 +30,26 @@ class mainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
 
-        self.cb_connected.setChecked(nordvpn.isConnected(nordvpn))
+        # QTimer for the GUI updater
+        self.timer = QtCore.QTimer(self)
+        self.timer.setInterval(100) # time in ms
+        self.timer.timeout.connect(self.__update)
+        self.timer.start()
+
 
         print("Installed: " + str(nordvpn.checkInstall(nordvpn)))
         print("Connected: " + str(nordvpn.isConnected(nordvpn)))
+
+    ##
+    # @private
+    # @brief    Updater
+    # @details  This private method is called periodically by the QTimer
+    @QtCore.pyqtSlot()
+    def __update(self):
+        # update the status of the vpn connection
+        self.cb_connected.setChecked(nordvpn.isConnected(nordvpn))
+
+
+
 
 
