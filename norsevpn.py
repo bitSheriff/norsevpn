77b1e67a891fca860bd1a0537d4f9b9845de37b9
bitSheriff <bitSheriff@protonmail.com>
2021-11-28 09:37:11 +0100

    added material dark as Qt style

--- norsevpn.py
+++ norsevpn.py
@@ -5,6 +5,7 @@ import sys
 import os
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog
+import qdarkstyle
 
 # own modules and libs
 #import vizu.norseVPNGUIWindow as norseVPNGUIWindow
@@ -19,5 +20,9 @@ if __name__ == "__main__":
     """! Initializes the program."""
     app = QtWidgets.QApplication(sys.argv)
     window = mainWindow.mainWindow()
+
+    # set the window style
+    app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
+
     window.show()
     sys.exit(app.exec_())
