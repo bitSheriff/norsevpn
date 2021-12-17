5f05d58aacecb7258b9f4e71750eb817706d598c
bitSheriff <bitsheriff@protonmail.com>
2021-12-17 21:05:40 +0100

    first try of pyinstaller

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# clean the build enviroment
+rm -rf dist/
+rm -rf build/
+
 # activate the virtual enviroment
-source .venve/bin/activate
+source .venv/bin/activate
 
 # export the needed packages
 pip3 freeze > requirements.txt
+
+pyinstaller norsevpn.spec \
+--onefile \
+--noconsole \
+--icon=doc/img/logo/norsevpn.ico \
+--add-data="README.md:." \
+--add-data="vizu/norsevpn.ui:." \
+--add-data="vizu/config.ui:." \
+--add-data="vizu/info.ui:." \
+--paths ./vizu
